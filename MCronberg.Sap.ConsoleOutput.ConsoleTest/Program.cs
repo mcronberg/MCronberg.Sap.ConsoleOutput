37d951cc317d882f000bd7abc0925b3cf99d2a0a
Michell <Admin@TA-822-INS>
2022-05-17 13:02:48 +0200

    init

---
+++ MCronberg.Sap.ConsoleOutput.ConsoleTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using MCronberg.Sap.ConsoleOutput.Core;
+using System;
+
+namespace MCronberg.Sap.ConsoleOutput.ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer w = new Writer();
+            w.BigHeader("test");
+            w.BigHeader("test", '-');
+            w.BigHeader("test", '-', ConsoleColor.Red);
+
+            w.SimpleHeader("test");
+            w.SimpleHeader("test", ConsoleColor.Red);
+
+            w.Write("test");
+            w.Write("test", ConsoleColor.Red);
+            w.NewLine();
+            w.Json(new { a = 1, b = "b" });
+
+            Exception e = new ArgumentException();
+            w.FullError(e);
+            w.FullError(e, ConsoleColor.Red);
+            w.SimpleError(e);
+            w.SimpleError(e, ConsoleColor.Red);
+
+            if (System.IO.Directory.Exists(@"c:\temp"))
+            {
+                w = new Writer(t => System.IO.File.AppendAllText(@"c:\temp\test.txt", t + "\r\n"));
+                w.BigHeader("Text to file");
+                w.BigHeader("Text to file");
+
+                w = new Writer(t => System.IO.File.AppendAllText(@"c:\temp\test.txt", t + "\r\n"), true);
+                w.BigHeader("Text to file and console");
+                w.BigHeader("Text to file and console");
+
+            }
+
+        }
+    }
+}
---
+++ MCronberg.Sap.ConsoleOutput.Core/Writer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCronberg.Sap.ConsoleOutput.Core
+{
+    public class Writer
+    {
+        private readonly Action<string> writer;
+
+        public Writer(Action<string> writer = null, bool addConsole = false)
+        {
+            this.writer = writer;
+            if (this.writer == null)
+                this.writer = Console.WriteLine;
+            else {
+                if (addConsole)
+                    this.writer += Console.WriteLine;
+            }
+        }
+
+        private string GetJson(object obj, bool writeIndented = true)
+        {
+            return System.Text.Json.JsonSerializer.Serialize(obj, new System.Text.Json.JsonSerializerOptions { WriteIndented = writeIndented });
+        }
+
+        public void Write()
+        {
+            Write("");
+
+        }
+        public void Write(string txt, ConsoleColor color = ConsoleColor.Gray)
+        {
+            ConsoleColor c = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            writer(txt);
+            Console.ForegroundColor = c;
+        }
+
+        public void FullError(Exception ex, ConsoleColor color = ConsoleColor.Gray)
+        {
+            BigHeader(ex.Message, '*', color);
+            Write(ex.ToString(), color);
+        }
+
+        public void SimpleError(Exception ex, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Write(ex.ToString(), color);
+        }
+
+        public void SimpleHeader(string header, ConsoleColor color = ConsoleColor.Gray)
+        {
+            var prePost = new String('-', header.Length);
+
+            Write(header, color);
+            Write(prePost, color);
+            NewLine();
+        }
+
+        public void BigHeader(string header, char headerChar = '=', ConsoleColor color = ConsoleColor.Gray)
+        {
+            var prePost = new String(headerChar, header.Length);
+            Write(prePost, color);
+            Write(header.ToUpper(), color);
+            Write(prePost, color);
+            NewLine();
+        }
+
+        public void Json(object obj, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Write(GetJson(obj, true), color);
+        }
+
+        public void WriteJsonToFile(object obj, string path)
+        {
+            Write("Saving json to " + path + " for inspection");
+            try
+            {
+                string json = GetJson(obj, true);
+                System.IO.File.WriteAllText(path, json);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error writing to " + path, ex);
+            }
+        }
+
+        public void NewLine(int count = 1)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Write("");
+            }
+        }
+
+    }
+}
