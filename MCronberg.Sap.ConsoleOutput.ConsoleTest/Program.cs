cb24a1cfcbe49f19496c621e3201166a1afe3bfa
Michell Cronberg <michell@cronberg.dk>
2022-06-14 19:31:02 +0200

    tostring

--- MCronberg.Sap.ConsoleOutput.ConsoleTest/Program.cs
+++ MCronberg.Sap.ConsoleOutput.ConsoleTest/Program.cs
@@ -28,11 +28,16 @@ namespace MCronberg.Sap.ConsoleOutput.ConsoleTest
             w.SimpleError(e);
             w.SimpleError(e, ConsoleColor.Red);
 
+
+            w.Table<Test>(Test.GetTest());
+            w.Table<Test>(Test.GetTest(), true);
             w.Table<Test>(Test.GetTest(), i => i.A, i => i.B);
             w.Table<Test>(Test.GetTest(), showCounter: true, i => i.A, i => i.B);
             List<int> lst1 = new List<int>() { 4, 5, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1 };
             w.Table(lst1, true);
+            
             w.Table(values: lst1, columnHeaders: new[] { "i" }, showCounter: true, i => i);
+
 
             if (System.IO.Directory.Exists(@"c:\temp"))
             {
@@ -43,7 +48,6 @@ namespace MCronberg.Sap.ConsoleOutput.ConsoleTest
                 w = new Writer(t => System.IO.File.AppendAllText(@"c:\temp\test.txt", t + "\r\n"), true);
                 w.BigHeader("Text to file and console");
                 w.BigHeader("Text to file and console");
-
             }
 
         }
@@ -53,13 +57,14 @@ namespace MCronberg.Sap.ConsoleOutput.ConsoleTest
     {
         public int A { get; set; }
         public string B { get; set; }
+        public string C { get; set; }
 
         public static List<Test> GetTest()
         {
             List<Test> lst = new List<Test>();
-            lst.Add(new Test { A = 1, B = "a" });
-            lst.Add(new Test { A = 2, B = "b" });
-            lst.Add(new Test { A = 3, B = "c" });
+            lst.Add(new Test { A = 1, B = "a", C = "x" });
+            lst.Add(new Test { A = 2, B = "b",C = null });
+            lst.Add(new Test { A = 3, B = "c", C = "y" });
             return lst;
         }
     }
--- MCronberg.Sap.ConsoleOutput.Core/Writer.cs
+++ MCronberg.Sap.ConsoleOutput.Core/Writer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 // See https://github.com/Robert-McGinley/TableParser for table
@@ -125,12 +124,12 @@ namespace MCronberg.Sap.ConsoleOutput.Core
 
         public void Table<T>(IEnumerable<T> values, string[] columnHeaders, bool showCounter = false, params Func<T, object>[] valueSelectors)
         {
-            TableParser.TableParserHelper tableParser = new TableParser.TableParserHelper(showCounter) ;
+            TableParser.TableParserHelper tableParser = new TableParser.TableParserHelper(showCounter);
             var txt = tableParser.ToStringTable<T>(values, columnHeaders, valueSelectors);
             Write(txt);
         }
 
-        public void Table<T>(IEnumerable<T> values, string[] columnHeaders,  params Func<T, object>[] valueSelectors)
+        public void Table<T>(IEnumerable<T> values, string[] columnHeaders, params Func<T, object>[] valueSelectors)
         {
             TableParser.TableParserHelper tableParser = new TableParser.TableParserHelper(false);
             var txt = tableParser.ToStringTable<T>(values, columnHeaders, valueSelectors);
@@ -141,149 +140,36 @@ namespace MCronberg.Sap.ConsoleOutput.Core
         public void Table<T>(IEnumerable<T> values, bool showCounter = false)
         {
             TableParser.TableParserHelper tableParser = new TableParser.TableParserHelper(showCounter);
-            var txt = tableParser.ToStringTable<T>(values, new[] { "Value" }, i => i);
-            Write(txt);
+            if (typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance).Length > 1)
+            {
+                var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                string[,] v = new string[values.Count() + 1, properties.Count()];
+                int row = 0, col = 0;
+                foreach (var propery in properties)
+                {
+                    v[row, col++] = propery.Name;
+                }
+                row++;
+                col = 0;
+                foreach (var value in values)
+                {
+                    foreach (var propery in properties)
+                    {
+                        v[row, col++] = propery.GetValue(value) == null ? "" : propery.GetValue(value).ToString();
+                    }
+                    row++;
+                    col = 0;
+                }
+                var txt = tableParser.ToStringTable(v);
+                Write(txt);
+
+            }
+            else
+            {
+                var txt = tableParser.ToStringTable<T>(values, new[] { "Value" }, i => i);
+                Write(txt);
+            }
         }
 
-    }
-
-    // Inspired/copied from https://github.com/Robert-McGinley/TableParser    
-    namespace TableParser
-    {
-        using System;
-        using System.Collections.Generic;
-        using System.Diagnostics;
-        using System.Linq;
-        using System.Linq.Expressions;
-        using System.Reflection;
-        using System.Text;
-
-        internal class TableParserHelper
-        {
-            private readonly bool showCounter;
-
-            public TableParserHelper(bool showCounter = false)
-            {
-                this.showCounter = showCounter;
-            }
-            public string ToStringTable<T>(IEnumerable<T> values, string[] columnHeaders, params Func<T, object>[] valueSelectors)
-            {
-                return ToStringTable(values.ToArray(), columnHeaders, valueSelectors);
-            }
-
-            public string ToStringTable<T>(T[] values, string[] columnHeaders, params Func<T, object>[] valueSelectors)
-            {
-                
-
-                var arrValues = new string[values.Length + 1, valueSelectors.Length];
-
-                // Fill headers
-                for (int colIndex = 0; colIndex < arrValues.GetLength(1); colIndex++)
-                {
-                    arrValues[0, colIndex] = columnHeaders[colIndex];
-                }
-
-                // Fill table rows
-                for (int rowIndex = 1; rowIndex < arrValues.GetLength(0); rowIndex++)
-                {
-                    for (int colIndex = 0; colIndex < arrValues.GetLength(1); colIndex++)
-                    {
-                        object value = valueSelectors[colIndex].Invoke(values[rowIndex - 1]);
-
-                        arrValues[rowIndex, colIndex] = value != null ? value.ToString() : "null";
-                    }
-                }
-
-                return ToStringTable(arrValues);
-            }
-
-            public string ToStringTable(string[,] arrValues)
-            {
-                int[] maxColumnsWidth = GetMaxColumnsWidth(arrValues);
-                int countWidth = arrValues.GetLength(0).ToString().Length;
-                int addShowCounter = showCounter ? countWidth +2 : 0;
-                var headerSpliter = new string('-', maxColumnsWidth.Sum(i => i + 3) - 1 + addShowCounter+1);
-
-                var sb = new StringBuilder();
-                int count = -1;
-                for (int rowIndex = 0; rowIndex < arrValues.GetLength(0); rowIndex++)
-                {
-                    if (showCounter)
-                    {
-                        sb.Append(" | ");
-                        if (rowIndex == 0)
-                            sb.Append("#" + new string(' ', countWidth -1));                         
-                        else
-                            sb.Append(count.ToString().PadRight(countWidth));
-                    }
-
-                    for (int colIndex = 0; colIndex < arrValues.GetLength(1); colIndex++)
-                    {
-                        // Print cell
-                        string cell = arrValues[rowIndex, colIndex];
-                        cell = cell.PadRight(maxColumnsWidth[colIndex]);
-                        sb.Append(" | ");
-                        sb.Append(cell);
-                    }
-                    // Print end of line
-                    sb.Append(" | ");
-                    sb.AppendLine();
-
-                    // Print splitter
-                    if (rowIndex == 0)
-                    {
-                        sb.AppendFormat(" |{0}| ", headerSpliter);
-                        sb.AppendLine();
-                    }
-                    count++;
-                }
-
-                return sb.ToString();
-            }
-
-            private int[] GetMaxColumnsWidth(string[,] arrValues)
-            {
-                var maxColumnsWidth = new int[arrValues.GetLength(1)];
-                for (int colIndex = 0; colIndex < arrValues.GetLength(1); colIndex++)
-                {
-                    for (int rowIndex = 0; rowIndex < arrValues.GetLength(0); rowIndex++)
-                    {
-                        int newLength = arrValues[rowIndex, colIndex].Length;
-                        int oldLength = maxColumnsWidth[colIndex];
-
-                        if (newLength > oldLength)
-                        {
-                            maxColumnsWidth[colIndex] = newLength;
-                        }
-                    }
-                }
-
-                return maxColumnsWidth;
-            }
-
-            public string ToStringTable<T>(IEnumerable<T> values, params Expression<Func<T, object>>[] valueSelectors)
-            {
-                var headers = valueSelectors.Select(func => GetProperty(func).Name).ToArray();
-                var selectors = valueSelectors.Select(exp => exp.Compile()).ToArray();
-                return ToStringTable(values, headers, selectors);
-            }
-
-            private PropertyInfo GetProperty<T>(Expression<Func<T, object>> expresstion)
-            {
-                if (expresstion.Body is UnaryExpression)
-                {
-                    if ((expresstion.Body as UnaryExpression).Operand is MemberExpression)
-                    {
-                        return ((expresstion.Body as UnaryExpression).Operand as MemberExpression).Member as PropertyInfo;
-                    }
-                }
-
-                if ((expresstion.Body is MemberExpression))
-                {
-                    return (expresstion.Body as MemberExpression).Member as PropertyInfo;
-                }
-                return null;
-            }
-        }
     }
 }
